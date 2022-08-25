cc6673765137fa744f0f98d94d6d7a137237c3d4
timmyCturner <kuenytim@gmail.com>
2022-08-25 10:32:06 -0700

    public

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    import * as THREE from '/node_modules/three/build/three.module.js';
+    import * as THREE from './node_modules/three/build/three.module.js';
     import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
     import {SpaceShip} from './js/spaceship.js';
     import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
