0daca0c7f0b45cf3e5a403cee4ed1b5e2ec31999
timmyCturner <kuenytim@gmail.com>
2023-07-15 08:04:23 -0700

    build update crap

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
     import * as THREE from './node_modules/three/build/three.module.js';
     import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-    import {SpaceShip} from './js/spaceship.js';
+    //import {SpaceShip} from './js/spaceship.js';
     import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
     let camera, scene, renderer, controls, clock;
     let Ship;
